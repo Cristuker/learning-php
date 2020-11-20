42ff879bf94096927052af4662f36861c7f5215f
Cristian Silva <cristian123105@gmail.com>
2020-11-20 16:21:02 -0300

    finish

--- book-contacts.php
+++ book-contacts.php
@@ -28,15 +28,19 @@ while($option != 0 ){
             echo "\n";
             echo "Adicionar contatos\n\n";
             echo "Digite o nome: \n";
+            echo "-> ";
             $nome = readline();
             echo "\n";
             echo "Digite o telefone: \n";
+            echo "-> ";
             $telefone = readline();
             echo "\n";
             echo "Digite o email: \n";
+            echo "-> ";
             $email = readline();
             echo "\n";
             echo "Digite a data de nascimento no formato (11/01/1999): \n";
+            echo "-> ";
             $nasc = readline();
             echo "\n";
             $novoContato = new Contact($nome, $telefone, $email, $nasc);
@@ -51,6 +55,7 @@ while($option != 0 ){
             echo "Deletar contato";
             echo "\n\n";
             echo "Passe o id do contato: \n";
+            echo "-> ";
             $id = readline();
             unset($contatos[$id]);
             $contatos = array_values($contatos);
@@ -67,6 +72,71 @@ while($option != 0 ){
             break;
         case 4:
             echo "Editar contato";
+            echo "\n\n";
+            echo "Passe o id do contato: \n";
+            echo "-> ";
+            $id = readline();
+            echo "O que você deseja editar?: \n";
+            echo <<<OP
+            1. Nome
+            2. Telefone
+            3. Email
+            4. Data de Nascimento
+            5. Tudo
+            OP;
+            echo "-> ";
+            $option = readline();
+            switch ($option) {
+                case 1:
+                    echo "Novo nome: \n";
+                    echo "-> ";
+                    $novoNome = readline();
+                    $contatos[$id]->setNome($novoNome);
+                    break;
+                case 2:
+                    echo "Novo telefone: \n";
+                    echo "-> ";
+                    $novoTelefone = readline();
+                    $contatos[$id]->setTel($novoTelefone);
+                    break;
+                case 3:
+                    echo "Novo email: \n";
+                    echo "-> ";
+                    $novoEmail = readline();
+                    $contatos[$id]->setEmail($novoEmail);
+                    break;
+                case 4:
+                    echo "Novo Data de Nascimento: \n";
+                    echo "-> ";
+                    $novoNasc = readline();
+                    $contatos[$id]->setNasc();
+                    break;
+                case 5: 
+                    echo "Novo nome: \n";
+                    echo "-> ";
+                    $novoNome = readline();
+
+                    echo "Novo telefone: \n";
+                    echo "-> ";
+                    $novoTelefone = readline();
+
+                    echo "Novo email: \n";
+                    echo "-> ";
+                    $novoEmail = readline();
+
+                    echo "Novo Data de Nascimento: \n";
+                    echo "-> ";
+                    $novoNasc = readline();
+
+                    $contatos[$id] = new Contact($novoNome, $novoTelefone, $novoEmail, $novoNasc);
+                    break;
+                
+                default:
+                    echo "Insira uma opção válida...\n";
+                    break;
+            }
+            echo "\n\n";
+            echo "Contato editado com sucesso!";
             break;
         case 0:
             exit();
--- contato.php
+++ contato.php
@@ -5,13 +5,30 @@ class Contact {
     private $telefone;
     private $email;
     private $nasc;
-    function __construct($nome, $telefone, $email, $nasc)
-    {
+    
+    function __construct($nome, $telefone, $email, $nasc) {
         $this->nome = $nome;
         $this->telefone = $telefone;
         $this->email = $email;
         $this->nasc = $nasc;
     }
+
+    public function setNome($nome) {
+        $this->nome = $nome;
+    }
+
+    public function setTel($telefone) {
+        $this->telefone = $telefone;
+    }
+
+    public function setEmail($email) {
+        $this->email = $email;
+    }
+
+    public function setNasc($nasc) {
+        $this->nasc = $nasc;
+    }
+
     public function getInfos() {
         echo <<<INFO
         Nome: $this->nome 
