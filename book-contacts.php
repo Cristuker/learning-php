c5b777bae8245ca607f7862d9f03b75ad263647b
Cristian Silva <cristian123105@gmail.com>
2020-11-20 16:06:24 -0300

    deletando usuarios

--- book-contacts.php
+++ book-contacts.php
@@ -1,8 +1,5 @@
 <?php 
-
-
 require_once './contato.php';
-
 
 $contatos = array();
 $option = 9;
@@ -10,7 +7,9 @@ $option = 9;
 while($option != 0 ){
     echo "\n";
     echo <<<MENU
-    Livro de contatos
+    *********************
+    * Livro de contatos *
+    *********************
 
     1. Adicionar contato.
     2. Deletar contato.
@@ -30,28 +29,39 @@ while($option != 0 ){
             echo "Adicionar contatos\n\n";
             echo "Digite o nome: \n";
             $nome = readline();
+            echo "\n";
             echo "Digite o telefone: \n";
             $telefone = readline();
+            echo "\n";
             echo "Digite o email: \n";
             $email = readline();
+            echo "\n";
             echo "Digite a data de nascimento no formato (11/01/1999): \n";
             $nasc = readline();
+            echo "\n";
             $novoContato = new Contact($nome, $telefone, $email, $nasc);
             echo "\n \n";
             echo "Contato criado com sucesso \n";
             array_push($contatos, $novoContato);
             $novoContato->getInfos();
+            echo "\n \n";
 
             break;
         case 2:
             echo "Deletar contato";
+            echo "\n\n";
+            echo "Passe o id do contato: \n";
+            $id = readline();
+            unset($contatos[$id]);
+            $contatos = array_values($contatos);
             break;
         case 3:
             echo "Listar todos os contatos";
             echo "\n\n";
             for ($i=0; $i < sizeof($contatos) ; $i++) { 
+                echo "\nid: $i\n";
+                echo $contatos[$i]->getInfos();
                 echo "\n";
-                echo $contatos[0]->getInfos();
             }
             
             break;
